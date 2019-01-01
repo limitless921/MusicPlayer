2a2bb3232eb6fe8c949b68eeab425ebc2cbaff7a
Your Name <you@example.com>
2019-01-01 20:04:36 +0800

    the musicplayer

---
+++ app/src/main/java/com/d/music/event/eventbus/ProgressEvent.java
@@ -0,0 +1,7 @@
+package com.d.music.event.eventbus;
+
+
+public class ProgressEvent {
+    public int currentPosition;
+    public int duration;
+}
---
+++ app/src/main/java/com/d/music/setting/model/RadioModel.java
@@ -0,0 +1,12 @@
+package com.d.music.setting.model;
+
+
+public class RadioModel {
+    public final static int MODE_NORMAL = 0;
+    public final static int MODE_MINIMALIST = 1;
+    public final static int MODE_NOTIFICATION = 2;
+
+    public String content;
+    public int color;
+    public boolean isChecked;
+}
